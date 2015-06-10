8a1c6a63da9f2efa7688c3c9f2d1f743f76713c3
Christoph Graumann <c.graumann@tum.de>
2015-06-10 19:16:01 +0200

    MVG app basic intents and utterances

---
+++ mvg-request/intents.js
@@ -0,0 +1,21 @@
+{
+  "intents": [
+    {
+      "intent": "NextTrain",
+      "slots": [
+        {
+          "name": "Station",
+          "type": "LITERAL"
+        },
+        {
+          "name": "Direction",
+          "type": "LITERAL"
+        }
+      ]
+    },
+    {
+      "intent": "NextLocalTrain",
+      "slots": []
+    }
+  ]
+}
